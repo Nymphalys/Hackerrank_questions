e88cbce7abc525d3c21222f811a1741a09f1dd09
Ladli <kumariladli2882@gmail.com>
2022-02-22 19:54:02 +0530

    Create the_hurdle_race.cpp

---
+++ PROBLEM_SOLVING_INC++/the_hurdle_race.cpp
@@ -0,0 +1,132 @@
+/*A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the characters have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by 1 unit for each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already clear all of the hurdles, return 0.
+
+Example
+
+Height = [1,2,3,3,2]
+
+K = 1
+
+The character can jump 1 unit high initially and must take 3-1=2 doses of potion to be able to jump all of the hurdles.
+
+Function Description
+
+Complete the hurdleRace function in the editor below.
+
+hurdleRace has the following parameter(s):
+
+int k: the height the character can jump naturally
+int height[n]: the heights of each hurdle
+Returns
+
+int: the minimum number of doses required, always 0 or more
+Input Format
+
+The first line contains two space-separated integers  and , the number of hurdles and the maximum height the character can jump naturally.
+
+The second line contains  space-separated integers  where .
+
+Constraints
+
+1<=n, k<=100
+1<=height[i]<=100
+Sample Input 0
+
+5 4
+
+1 6 3 5 2
+Sample Output 0
+
+2*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+ int hurdleRace(int k, vector<int> height) {
+
+   sort(height.begin(), height.end());
+  int result = height[height.size()-1]-k;
+
+if(result < 0)
+   result =0; 
+
+return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string height_temp_temp;
+    getline(cin, height_temp_temp);
+
+    vector<string> height_temp = split(rtrim(height_temp_temp));
+
+    vector<int> height(n);
+
+    for (int i = 0; i < n; i++) {
+        int height_item = stoi(height_temp[i]);
+
+        height[i] = height_item;
+    }
+
+    int result = hurdleRace(k, height);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
