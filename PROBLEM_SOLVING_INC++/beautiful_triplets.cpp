1bc579c51232aa73d0df56e68e58c848e1b58d61
Ladli <kumariladli2882@gmail.com>
2022-02-27 20:37:29 +0530

    Create beautiful_triplets.cpp

---
+++ PROBLEM_SOLVING_INC++/beautiful_triplets.cpp
@@ -0,0 +1,119 @@
+/*Function Description
+
+Complete the beautifulTriplets function in the editor below. It must return an integer that represents the number of beautiful triplets in the sequence.
+
+beautifulTriplets has the following parameters:
+
+d: an integer
+arr: an array of integers, sorted ascending
+ 
+
+Input Format
+
+The first line contains 2 space-separated integers n and d, the length of the sequence and the beautiful difference.
+The second line contains n space-separated integers arr[i].
+
+Output Format
+
+Print a single line denoting the number of beautiful triplets in the sequence.
+
+Sample Input
+
+7 3
+1 2 4 5 7 8 10
+Sample Output
+
+3*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int beautifulTriplets(int d, vector<int> arr) {
+    unordered_map<int, int> m;
+    int cnt = 0;
+    for (int a : arr) {
+        m[a] += 1;
+        cnt += m[a-d]*m[a-2*d];
+    }
+    return cnt;
+}
+
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int d = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = beautifulTriplets(d, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
