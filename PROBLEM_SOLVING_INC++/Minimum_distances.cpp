294080793e357ebb2c964d48a35875a4afa59a19
Ladli <kumariladli2882@gmail.com>
2022-02-27 22:14:59 +0530

    Create Minimum_distances.cpp

---
+++ PROBLEM_SOLVING_INC++/Minimum_distances.cpp
@@ -0,0 +1,29 @@
+/*We define the distance between two array values as the number of indices between the two values. Given , find the minimum distance between any pair of equal elements in the array. If no such value exists, print .
+For example, if , there are two matching pairs of values: . The indices of the 's are  and , so their distance is . The indices of the 's are  and , so their distance is .
+
+Function Description
+
+Complete the minimumDistances function in the editor below. It should return the minimum distance between any two matching elements.
+
+minimumDistances has the following parameter(s):
+
+a: an array of integers
+Input Format
+
+The first line contains an integer , the size of array .
+The second line contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print a single integer denoting the minimum  in . If no such value exists, print .
+
+Sample Input
+
+6
+7 1 3 4 1 7
+Sample Output
+
+3*/
+
