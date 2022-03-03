4523092e7a09d7dfe3407561b11924e3a0aa2819
Ladli <kumariladli2882@gmail.com>
2022-03-03 23:48:15 +0530

    Create Larry's_array.cpp

---
+++ PROBLEM_SOLVING_INC++/Larry's_array.cpp
@@ -0,0 +1,150 @@
+/*Larry has been given a permutation of a sequence of natural numbers incrementing from 1 as an array. He must determine whether the array can be sorted using the following operation any number of times:
+
+Choose any 3 consecutive indices and rotate their elements in such a way that
+
+ABC->BCA->CAB->ABC.
+
+For example, A = {1,6,5,2,4,3}  :
+
+A		rotate 
+[1,6,5,2,4,3]	[6,5,2]
+[1,5,2,6,4,3]	[5,2,6]
+[1,2,6,5,4,3]	[5,4,3]
+[1,2,6,3,5,4]	[6,3,5]
+[1,2,3,5,6,4]	[5,6,4]
+[1,2,3,4,5,6]
+
+YES
+On a new line for each test case, print YES if A can be fully sorted. Otherwise, print NO.
+
+Function Description
+
+Complete the larrysArray function in the editor below. It must return a string, either YES or NO.
+
+larrysArray has the following parameter(s):
+
+A: an array of integers
+Input Format
+
+The first line contains an integer t, the number of test cases.
+
+The next t pairs of lines are as follows:
+
+The first line contains an integer n, the length of A.
+The next line contains n space-separated integers A[i].
+Output Format
+
+For each test case, print YES if A can be fully sorted. Otherwise, print NO.
+
+Sample Input
+
+3
+3
+3 1 2
+4
+1 3 4 2
+5
+1 2 3 5 4
+Sample Output
+
+YES
+YES
+NO*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+string larrysArray(vector <int> A) {
+    
+    int iCnt=0;
+    for (int i=0;i<A.size()-1;i++)    {
+        for (int j=i+1;j<A.size();j++)  {
+            if (A[i]>A[j]) iCnt++;
+        }
+    }
+    return (iCnt % 2 == 0) ? "YES" : "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string A_temp_temp;
+        getline(cin, A_temp_temp);
+
+        vector<string> A_temp = split(rtrim(A_temp_temp));
+
+        vector<int> A(n);
+
+        for (int i = 0; i < n; i++) {
+            int A_item = stoi(A_temp[i]);
+
+            A[i] = A_item;
+        }
+
+        string result = larrysArray(A);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
